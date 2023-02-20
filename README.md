a127e3c649ac3a480bf73c252cf3486da6d4e4b3
d0wnsider <jmhandsamson@gmail.com>
2023-02-20 18:43:43 -0500

    Updated buttons to disable after game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+Simple project for The Odin Project curriculum.
+Live demo: https://d0wnsider.github.io/rock-paper-scissors/
--- rps.js
+++ rps.js
@@ -1,6 +1,5 @@
 function getComputerChoice() { // random choice rock, paper and scissors
     let randomNumber = Math.floor(Math.random() * 3); // random number 0-2
-    
     if (randomNumber === 0) {
         return 'Rock';
     }
@@ -14,53 +13,48 @@ function getComputerChoice() { // random choice rock, paper and scissors
 
 function playRound(playerSelection, computerSelection) { // play a single round of the game
     if (computerSelection === 'Rock' && playerSelection.toLowerCase() === 'scissors') { // Rock computer selection
-        console.log(computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase());
-        return computerSelection;
+        return computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     }
     else if (computerSelection === 'Rock' && playerSelection.toLowerCase() === 'paper') {
-        console.log(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection);
-        return playerSelection;
+        return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
     }
     else if (computerSelection === 'Rock' && playerSelection.toLowerCase() === 'rock') {
-        return console.log("Tie");
+        return 'Tie';
     }
     else if (computerSelection === 'Paper' && playerSelection.toLowerCase() === 'rock') { // Paper computer selection
-        console.log(computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase());
-        return computerSelection;
+        return computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     }
     else if (computerSelection === 'Paper' && playerSelection.toLowerCase() === 'scissors') {
-        console.log(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection);
-        return playerSelection;
+        return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
     }
     else if (computerSelection === 'Paper' && playerSelection.toLowerCase() === 'paper') {
-        return console.log("Tie");
+        return 'Tie';
     }
-    if (computerSelection === 'Scissors' && playerSelection.toLowerCase() === 'paper') { // Scissors computer selection
-        console.log(computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase());
-        return computerSelection;
+    else if (computerSelection === 'Scissors' && playerSelection.toLowerCase() === 'paper') { // Scissors computer selection
+        return computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     }
     else if (computerSelection === 'Scissors' && playerSelection.toLowerCase() === 'rock') {
-        console.log(playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection);
-        return playerSelection;
+        return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase() + " beats " + computerSelection;
     }
     else if (computerSelection === 'Scissors' && playerSelection.toLowerCase() === 'scissors') {
-        return console.log("Tie");   
+        return 'Tie';   
     }
 }
 
 // start of program
 let playerScore = 0;
 let computerScore = 0;
-let results;
+let playResults;
 let playerSelection;
 let computerSelection = getComputerChoice(); // returning either 'Rock', 'Paper', 'Scissors'
 
 // creating variables linking to DOM
-const rock = document.querySelector("#rock");
-const paper = document.querySelector("#paper");
-const scissors = document.querySelector("#scissors");
-const container = document.querySelector("#container");
-
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const container = document.querySelector('#container');
+const finalResult = document.querySelector('#finalResult');
+let results = document.querySelector('#results');
 const playerScoreContainer = document.createElement('div');
 const computerScoreContainer = document.createElement('div');
 container.appendChild(playerScoreContainer);
@@ -69,80 +63,95 @@ container.appendChild(computerScoreContainer);
 function game() {
     // player adding event listener
     rock.addEventListener('click', () => {
-        results = playRound("rock", computerSelection); // win on rock player selection
+        playResults = playRound('rock', computerSelection); // win on rock player selection
         //check if computer is what
         if (computerSelection === 'Paper') { // if 
             computerScore += 1;
         }
-        else if (computerSelection === 'Scissors') {
+        if (computerSelection === 'Scissors') {
             playerScore += 1;
         }
-        else {
-            playerScore += 1;
-            computerScore += 1;
-        }
-        playerScoreContainer.textContent = playerScore;
-        computerScoreContainer.textContent = computerScore;
+
+        playerScoreContainer.textContent = "Player score: " + playerScore;
+        computerScoreContainer.textContent = "Computer score: " + computerScore;
         // reset computer selection for next round
         computerSelection = getComputerChoice();
+        // print results every round
+        results.textContent = playResults;
         // checks if player or computer won 
         if (playerScore == 5) {
-            console.log("Player won!");
+            finalResult.textContent = "Player wins!";
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
         }
         if (computerScore == 5) {
-            console.log("Computer won!")
+            finalResult.textContent = "Computer wins!";
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
         }
     });
     paper.addEventListener('click', () => {
-        playerSelection = "paper";
-        results = playRound(playerSelection, computerSelection); // win on rock player selection
+        playerSelection = 'paper';
+        playResults = playRound(playerSelection, computerSelection); // win on rock player selection
         //check if player or computer won on 1 round
         if (computerSelection === 'Rock') {    
             playerScore += 1;
         }
-        else if (computerSelection === 'Scissors') {
+        if (computerSelection === 'Scissors') {
             computerScore += 1;
         }
-        else {
-            playerScore += 1;
-            computerScore += 1;
-        }
-        playerScoreContainer.textContent = playerScore;
-        computerScoreContainer.textContent = computerScore;
+
+        playerScoreContainer.textContent = "Player score: " + playerScore;
+        computerScoreContainer.textContent = "Computer score: " + computerScore;
         // reset computer selection for next round
         computerSelection = getComputerChoice();
+        // print results every round
+        results.textContent = playResults;
         // checks if player or computer won 
         if (playerScore == 5) {
-            console.log("Player won!");
+            finalResult.textContent = "Player wins!";
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
         }
         if (computerScore == 5) {
-            console.log("Computer won!")
+            finalResult.textContent = "Computer won!";
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
         }
     });
     scissors.addEventListener('click', () => {
-        playerSelection = "scissors";
-        results = playRound(playerSelection, computerSelection); // win on rock player selection
+        playerSelection = 'scissors';
+        playResults = playRound(playerSelection, computerSelection); // win on rock player selection
         //check if player or computer won on 1 round
         if (computerSelection === 'Rock') {    
             computerScore += 1;
         }
-        else if (computerSelection === 'Paper') {
+        if (computerSelection === 'Paper') {
             playerScore += 1;
         }
-        else {
-            playerScore += 1;
-            computerScore += 1;
-        }
-        playerScoreContainer.textContent = playerScore;
-        computerScoreContainer.textContent = computerScore;
+        
+        playerScoreContainer.textContent = "Player score: " + playerScore;
+        computerScoreContainer.textContent = "Computer score: " + computerScore;
         // reset computer selection for next round
         computerSelection = getComputerChoice();
+        // print results every round
+        results.textContent = playResults;
         // checks if player or computer won 
         if (playerScore == 5) {
-            console.log("Player won!");
+            finalResult.textContent = "Player wins!";
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
         }
         if (computerScore == 5) {
-            console.log("Computer won!")
+            finalResult.textContent = "Computer wins!";
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
         }
     });
 }
